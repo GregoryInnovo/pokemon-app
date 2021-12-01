8a6f18c897c8c0b9a10780901c24bbf38bc96a5d
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-12-01 14:12:23 -0500

    get pokémons info

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { getPokemonsApi } from "../api/pokemon";
+import { getPokemonsApi, getPokemonDetailsByUrlApi } from "../api/pokemon";
 export default function Pokedex() {
+  // state and function to setState  = initialState
+  const [pokemons, setPokemons] = useState([]);
+  console.log(pokemons);
   useEffect(() => {
     // ()() function anonima auto ejecutada
     (async () => {
@@ -13,7 +16,21 @@ export default function Pokedex() {
   const loadPokemons = async () => {
     try {
       const response = await getPokemonsApi();
-      console.log(response);
+
+      const pokemonsArray = [];
+      for await (const pokemon of response.results) {
+        const pokemonDetailts = await getPokemonDetailsByUrlApi(pokemon.url);
+        pokemonsArray.push({
+          id: pokemonDetailts.id,
+          name: pokemonDetailts.name,
+          type: pokemonDetailts.types[0].type.name,
+          order: pokemonDetailts.order,
+          image:
+            pokemonDetailts.sprites.other["official-artwork"].front_default,
+        });
+      }
+      // if the split operator dont exist we are saying ave an array in an array
+      setPokemons([...pokemons, ...pokemonsArray]);
     } catch (error) {
       console.log(error);
     }
