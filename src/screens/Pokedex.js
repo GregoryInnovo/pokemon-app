0cac0d29b6f1dafc6cb688e71809c51165b6d48f
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-11-30 23:29:03 -0500

    get pokemon from an api

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import { getPokemonsApi } from "../api/pokemon";
 export default function Pokedex() {
+  useEffect(() => {
+    // ()() function anonima auto ejecutada
+    (async () => {
+      await loadPokemons();
+    })();
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonsApi();
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <SafeAreaView>
       <Text>Pokédex</Text>
