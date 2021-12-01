029a0cb758ecc91946f82d96e545df2c87e0eefa
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-11-30 20:36:24 -0500

    install and config react navigation

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
-import { StatusBar } from "expo-status-bar";
+import "react-native-gesture-handler";
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import { StyleSheet, Text, View } from "react-native";
 import LoginForm from "./src/components/LoginForm";
 import Saludar from "./src/components/Saludar";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <LoginForm />
-      <Saludar firstName="Gregson" lastName="Murcia" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        {/* <LoginForm /> */}
+        <Saludar firstName="Gregson" lastName="Murcia Castro" />
+      </View>
+    </NavigationContainer>
   );
 }
 
