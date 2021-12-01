c1c413bbf11ef67536a251a74e4eb7fbe3e389db
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-11-30 20:11:57 -0500

    propTypes validation

--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text } from "react-native";
+import PropTypes from "prop-types";
 
 export default function Saludar(props) {
   const { firstName, lastName } = props;
@@ -12,7 +13,13 @@ export default function Saludar(props) {
   );
 }
 
-Saludar.defaultProps = {
-  firstName: "Gregson",
-  lastName: "Murcia Castro",
+// Saludar.defaultProps = {
+//   firstName: "Gregson",
+//   lastName: "Murcia Castro",
+// };
+
+// PropTypes help to a new programmer which values are required
+Saludar.propTypes = {
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,
 };
