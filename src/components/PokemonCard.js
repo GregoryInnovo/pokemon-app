58955b7d6ac0565bf6d2a16badf98cebd13180fb
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-12-01 22:02:07 -0500

    classification color and add package lodash

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -6,9 +6,13 @@ import {
   Image,
   TouchableWithoutFeedback,
 } from "react-native";
+import { capitalize } from "lodash";
+import getColorByPokemonType from "../utils/getColorByPokemonType";
 
 export default function PokemonCard(props) {
   const { pokemon } = props;
+  const pokemonColor = getColorByPokemonType(pokemon.type);
+  const bgStyles = { backgroundColor: pokemonColor, ...styles.bgStyles };
 
   const goToPokemon = () => {
     console.log(pokemon.name);
@@ -18,11 +22,11 @@ export default function PokemonCard(props) {
     <TouchableWithoutFeedback onPress={goToPokemon}>
       <View style={styles.card}>
         <View style={styles.spacing}>
-          <View style={styles.bgStyles}>
+          <View style={bgStyles}>
             <Text style={styles.number}>
               #{`${pokemon.order}`.padStart(3, 0)}
             </Text>
-            <Text style={styles.name}>{pokemon.name}</Text>
+            <Text style={styles.name}>{capitalize(pokemon.name)}</Text>
             <Image source={{ uri: pokemon.image }} style={styles.image} />
           </View>
         </View>
@@ -41,7 +45,9 @@ const styles = StyleSheet.create({
     padding: 5,
   },
   bgStyles: {
-    backgroundColor: "grey",
+    flex: 1,
+    borderRadius: 15,
+    padding: 10,
   },
   number: {
     position: "absolute",
--- src/navigation/PokedexNavigation.js
+++ src/navigation/PokedexNavigation.js
@@ -8,7 +8,11 @@ const Stack = createStackNavigator();
 export default function PokedexNavigation() {
   return (
     <Stack.Navigator initialRouteName="Pokédex">
-      <Stack.Screen name="Pokédex" component={PokedexScreen} />
+      <Stack.Screen
+        name="Pokédex"
+        component={PokedexScreen}
+        options={{ headerShown: false }}
+      />
       <Stack.Screen name="Pokémon" component={PokemonScreen} />
     </Stack.Navigator>
   );
