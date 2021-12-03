c70c29d46e4331c38eb5cc42c0373d17b6757e63
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-12-02 23:24:28 -0500

    improve login styles

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -1,13 +1,46 @@
 import React from "react";
-import { View, Text, Button, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  StyleSheet,
+  Keyboard,
+} from "react-native";
 
 export default function LoginForm() {
   return (
     <View>
-      <Text>Component Login Form</Text>
-      <TextInput placeholder="Email" />
-      <TextInput placeholder="Contraseña" />
-      {/* <Button tittle="Enviar" onPres={() => console.log("Me presionaste")} /> */}
+      <Text style={styles.title}>Iniciar sesión</Text>
+      <TextInput
+        placeholder="Nombre de usuario"
+        style={styles.input}
+        autoCapitalize="none"
+      />
+      <TextInput
+        placeholder="Contraseña"
+        style={styles.input}
+        autoCapitalize="none"
+        secureTextEntry={true}
+      />
+      <Button title="Entrar" onPress={() => console.log("Enviar")} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: "center",
+    fontSize: 28,
+    fontWeight: "bold",
+    marginTop: 50,
+    marginBottom: 15,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+});
