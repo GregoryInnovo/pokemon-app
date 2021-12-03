31a1fbb12fbc8e74ac16c4a0d301c622221fdff6
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-12-02 19:20:17 -0500

    config pokemon data

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getPokemonDetailsApi } from "../api/pokemon";
 import { View, Text } from "react-native";
 
 export default function Pokemon(props) {
-  const { route, navigation } = props;
+  const {
+    navigation,
+    route: { params },
+  } = props;
+
+  const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getPokemonDetailsApi(params.id);
+        setPokemon(response);
+      } catch (error) {
+        navigation.goBack();
+      }
+    })();
+  }, [params]);
+
+  if (!pokemon) return null;
+
   return (
     <View>
       <Text>Estamos mostrando un pokemon</Text>
+      <Text>{pokemon.name}</Text>
     </View>
   );
 }
