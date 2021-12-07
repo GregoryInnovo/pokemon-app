19e5dd06280a3db66f643c59ad2ad33a6163caa7
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-12-06 20:35:47 -0500

    Add icon favorite and validation in pokemon screen

---
+++ src/components/Pokemon/Favorite.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Icon from "react-native-vector-icons/FontAwesome5";
+
+export default function Favorite(props) {
+  const { id } = props;
+  const addFavorite = () => {
+    console.log("Se presiono", id);
+  };
+
+  return (
+    <Icon
+      name="heart"
+      color="#fff"
+      size={20}
+      onPress={addFavorite}
+      style={{ marginRight: 20 }}
+    />
+  );
+}
