b70479a4af7d8520cc6ee12952c5e5e58f43d4dc
Gregson Murcia Castro <gregoryinnovo@gmail.com>
2021-12-06 21:21:40 -0500

    Aysnc storage pokemon favorite implementation

--- src/components/Pokemon/Favorite.js
+++ src/components/Pokemon/Favorite.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Icon from "react-native-vector-icons/FontAwesome5";
+import { addPokemonFavoriteApi } from "../../api/favorite";
 
 export default function Favorite(props) {
   const { id } = props;
-  const addFavorite = () => {
+
+  const addFavorite = async () => {
+    await addPokemonFavoriteApi(id);
     console.log("Se presiono", id);
   };
 
--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { getPokemonFavoriteApi } from "../api/favorite";
 
 export default function Favorite() {
+  const checkFavorite = async () => {
+    const response = await getPokemonFavoriteApi();
+    console.log(response);
+  };
+
   return (
     <SafeAreaView>
       <Text>Favorite</Text>
+      <Button title="check" onPress={checkFavorite} />
     </SafeAreaView>
   );
 }
